cd31961c46e8bc8e8a94e3dc0aa0920bf575e352
aeonincode <khashayar.sasannia@hotmail.com>
2020-09-01 00:07:37 +0200

    Moving Away from Static Members and Moving Static fields

---
+++ src/com/codewithaeon/MortgageCalculator.java
@@ -0,0 +1,42 @@
+package com.codewithaeon;
+
+public class MortgageCalculator {
+    public final static byte MONTHS_IN_YEAR = 12;
+    public final static byte PERCENT = 100;
+    
+    private int principal;
+    private float annualInterest;
+    private byte years;
+
+    public MortgageCalculator(int principal, float annualInterest, byte years) {
+        this.principal = principal;
+        this.annualInterest = annualInterest;
+        this.years = years;
+    }
+
+    public double calculateBalance(short numberOfPaymentsMade) {
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+
+        double balance = principal
+                * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return balance;
+    }
+
+    public double calculateMortgage() {
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return mortgage;
+    }
+
+    public short getYears() {
+        return years;
+    }
+}
